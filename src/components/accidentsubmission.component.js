e6a2dae5e46d69d55d17f04b5cba50874b82055a
Dulana Peiris <dulthar@gmail.com>
2020-11-08 00:01:20 +0530

    Accident submission form completed

---
+++ src/components/accidentsubmission.component.js
@@ -0,0 +1,362 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
+export default class AccidentSubmission extends Component {
+  constructor(props) {
+    super(props);
+
+
+    this.onChangeAccidentDate = this.onChangeAccidentDate.bind(this);
+
+    this.onChangeHours = this.onChangeHours.bind(this);
+    this.onChangeMinutes = this.onChangeMinutes.bind(this);
+
+    this.onChangeDriverAge = this.onChangeDriverAge.bind(this);
+    this.onChangeDriverGender = this.onChangeDriverGender.bind(this);
+
+    this.onChangeWeather = this.onChangeWeather.bind(this);
+    this.onChangeVehicleType = this.onChangeVehicleType.bind(this);
+    this.onChangeVehicleYOM = this.onChangeVehicleYOM.bind(this);
+
+
+    this.onChangeLicenseIssueDate = this.onChangeLicenseIssueDate.bind(this);
+
+    this.onChangeDrivingSide = this.onChangeDrivingSide.bind(this);
+    this.onChangeSeverity = this.onChangeSeverity.bind(this);
+    this.onChangeReason = this.onChangeReason.bind(this);
+
+    this.onChangeKmPost = this.onChangeKmPost.bind(this);
+    this.onChangeSuburb = this.onChangeSuburb.bind(this);
+    this.onChangeOperatedSpeed = this.onChangeOperatedSpeed.bind(this);
+
+    this.onSubmit = this.onSubmit.bind(this);
+
+    const d= new Date();
+
+    this.state = {
+      hours: d.getHours(),
+      minutes: d.getMinutes(),
+      accidentDate:d,
+      driverAge: 0,
+      driverGender: 'male',
+      weather: 'clear',
+      vehicleType: 'car',
+      vehicleYOM: d.getFullYear(),
+      licenseIssueDate:d,
+      drivingSide: 'colombo',
+      severity: 'propertydamage',
+      reason: 'speed',
+      kmPost: 0,
+      suburb: '',
+      operatedSpeed: 0,
+
+      token:this.props.token,
+      res:''
+    }
+  }
+
+
+  onChangeAccidentDate(date) {
+      this.setState({
+        accidentDate: date
+      })
+    }
+
+    onChangeLicenseIssueDate(date) {
+        this.setState({
+          licenseIssueDate: date
+        })
+      }
+
+  onChangeHours(e) {
+    this.setState({
+      hours: e.target.value,
+      res:''
+    })
+  }
+  onChangeMinutes(e) {
+    this.setState({
+      minutes: e.target.value,
+      res:''
+    })
+  }
+
+  onChangeDriverAge(e) {
+    this.setState({
+      driverAge: e.target.value,
+      res:''
+    })
+  }
+  onChangeDriverGender(e) {
+    this.setState({
+      driverGender: e.target.value,
+      res:''
+    })
+  }
+
+  onChangeWeather(e) {
+    this.setState({
+      weather: e.target.value,
+      res:''
+    })
+  }
+
+  onChangeVehicleType(e) {
+    this.setState({
+      vehicleType: e.target.value,
+      res:''
+    })
+  }
+  onChangeVehicleYOM(e) {
+    this.setState({
+      vehicleYOM: e.target.value,
+      res:''
+    })
+  }
+
+
+  onChangeDrivingSide(e) {
+    this.setState({
+      drivingSide: e.target.value,
+      res:''
+    })
+  }
+  onChangeSeverity(e) {
+    this.setState({
+      severity: e.target.value,
+      res:''
+    })
+  }
+  onChangeReason(e) {
+    this.setState({
+      reason: e.target.value,
+      res:''
+    })
+  }
+  onChangeKmPost(e) {
+    this.setState({
+      kmPost: e.target.value,
+      res:''
+    })
+  }
+  onChangeSuburb(e) {
+    this.setState({
+      suburb: e.target.value,
+      res:''
+    })
+  }
+  onChangeOperatedSpeed(e) {
+    this.setState({
+      operatedSpeed: e.target.value,
+      res:''
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+
+    const accident = {
+      datetime:this.state.accidentDate,
+      driverAge: this.state.driverAge,
+      driverGender: this.state.driverGender,
+      weather: this.state.weather,
+      vehicleType: this.state.vehicleType,
+      vehicleYOM: this.state.vehicleYOM,
+      licenseIssueDate: this.state.licenseIssueDate,
+      drivingSide: this.state.drivingSide,
+      severity: this.state.severity,
+      reason: this.state.reason,
+      kmPost: this.state.kmPost,
+      suburb: this.state.suburb,
+      operatedSpeed: this.state.operatedSpeed,
+
+      sessionToken: this.state.token,
+    }
+
+    console.log(accident);
+
+    const d= new Date();
+
+    axios.post('http://localhost:5000/accident/submit', accident)
+      .then(res=>{
+        document.getElementById('accident-report-from').reset();
+        this.setState({
+          res: res.data.message,
+          accidentDate:d,
+          hours: d.getHours(),
+          minutes: d.getMinutes(),
+          driverAge: 0,
+          driverGender: 'male',
+          weather: 'clear',
+          vehicleType: 'car',
+          vehicleYOM: d.getFullYear(),
+          licenseIssueDay: d.getDay(),
+          licenseIssueMonth: d.getMonth(),
+          licenseIssueYear: d.getYear(),
+          licenseIssueDate: d,
+          drivingSide: 'colombo',
+          severity: 'propertydamage',
+          reason:'speed',
+          kmPost: 0,
+          suburb: '',
+          operatedSpeed: 0
+        })
+      })
+
+  }
+
+
+  render() {
+      return (
+      <div className="acc">
+
+
+      <form id="accident-report-from" onSubmit={this.onSubmit} >
+        <div className="header"><h3 style={{color:"black"}}>Accident Report </h3></div>
+        <br/>
+      <div>
+
+        <div className="form-group row">
+          <div className="col-sm-3"><label>Accident Date: </label></div>
+         <div className="col-sm-3"><div>
+              <DatePicker className="form-control"
+              selected={this.state.accidentDate}
+              onChange={this.onChangeAccidentDate}
+              />
+          </div>
+          </div>
+          </div>
+          <div className="form-group row">
+         <div className="col-sm-3"><label>Driver Age </label></div>
+          <div className="col-sm-3"><input  type="number"
+              className="form-control"
+              value={this.state.driverAge}
+              onChange={this.onChangeDriverAge}
+              min='0'
+              max='99'
+              /></div>
+          </div>
+          <div className="form-group row">
+          <div className="col-sm-3"><label>Driver Gender </label></div>
+          <div className="col-sm-3">
+              <select className="form-control" onChange={this.onChangeDriverGender}>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+              </select>
+              </div>
+          </div>
+          <div className="form-group row">
+          <div className="col-sm-3"><label>Weather </label></div>
+          <div className="col-sm-3">
+              <select className="form-control" onChange={this.onChangeWeather}>
+                  <option value="clear">Clear</option>
+                  <option value="rainy">Rainy</option>
+              </select>
+              </div>
+          </div>
+          <div className="form-group row">
+          <div className="col-sm-3"><label>Vehicle Type </label></div>
+          <div className="col-sm-3"><select className="form-control" onChange={this.onChangeVehicleType}>
+              <option value="car">Car</option>
+              <option value="van">Van</option>
+              <option value="hv">HV</option>
+              </select>
+          </div>
+          </div>
+          <div className="form-group row">
+          <div className="col-sm-3"><label>Vehicle manufacture year </label></div>
+          <div className="col-sm-3"><input  type="number"
+              className="form-control"
+              value={this.state.vehicleYOM}
+              onChange={this.onChangeVehicleYOM}
+              /></div>
+          </div>
+          <div className="form-group row">
+          <div className="col-sm-3"><label>License Issue Date </label></div>
+          <div className="col-sm-3"><DatePicker
+                  className="form-control"
+                  selected={this.state.licenseIssueDate}
+                  onChange={this.onChangeLicenseIssueDate}
+          /></div>
+          </div>
+          <div className="form-group row">
+          <div className="col-sm-3"><label>Driving Side </label></div>
+          <div className="col-sm-3"><select className="form-control" onChange={this.onChangeDrivingSide}>
+              <option value="colombo">Colombo to Matara</option>
+              <option value="matara">Matara to Colombo</option>
+              </select>
+          </div>
+          </div>
+          <div className="form-group row">
+          <div className="col-sm-3"><label>Severity </label></div>
+          <div className="col-sm-3"><select className="form-control" onChange={this.onChangeSeverity}>
+              <option value="propertydamage">Property Damage</option>
+              <option value="injury">Injury</option>
+              <option value="fatal">Fatal</option>
+              </select></div>
+          </div>
+          <div className="form-group row">
+          <div className="col-sm-3"><label>Reason </label></div>
+          <div className="col-sm-3"><select className="form-control" onChange={this.onChangeSeverity}>
+              <option value="speed">Speeding</option>
+              <option value="sleep">Sleep</option>
+              <option value="slipping">Slipping</option>
+              <option value="vehicleIssue">Vehicle issue</option>
+              <option value="tyreBlast">Tyre blast</option>
+              <option value="tyrePatch">Tyre patch</option>
+              <option value="animal">Animal Crossing</option>
+              <option value="other">Other</option>
+              </select></div>
+          </div>
+          <div className="form-group row">
+          <div className="col-sm-3"><label>KM Post </label></div>
+          <div className="col-sm-3"><input  type="number"
+              className="form-control"
+              value={this.state.kmPost}
+              onChange={this.onChangeKmPost}
+              min='0'
+              /></div>
+          </div>
+          <div className="form-group row">
+          <div className="col-sm-3"><label>Suburb </label></div>
+          <div className="col-sm-3"><input  type="text"
+              className="form-control"
+              value={this.state.suburb}
+              onChange={this.onChangeSuburb}
+              /></div>
+          </div>
+          <div className="form-group row">
+          <div className="col-sm-3"><label>Operated Speed (km/h) </label></div>
+          <div className="col-sm-3"><input  type="number"
+              className="form-control"
+              value={this.state.operatedSpeed}
+              onChange={this.onChangeOperatedSpeed}
+              min='0'
+              /></div>
+          </div>
+
+
+
+          <div className="form">
+            <br/>
+          <input type="submit" value="Submit Report" onSubmit={this.onSubmit} className="btn btn-primary" /> {this.state.res}
+          <div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+
+      </form>
+
+
+      </div>
+      )
+  }
+
+}
--- src/components/content.component.js
+++ src/components/content.component.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import AddPolice from './addpolice.component.js';
 import RemovePolice from './removepolice.component.js';
+import AccidentSubmission from './accidentsubmission.component.js';
 
 export default class Content extends React.Component {
 
@@ -14,6 +15,10 @@ export default class Content extends React.Component {
       case "removepolice":
       return (
         <RemovePolice token={this.props.token}/>
+      )
+      case "accidentsubmission":
+      return (
+        <AccidentSubmission token={this.props.token}/>
       )
       default:
       return(
