4220803383ee3ecce9d662448a0d838e575b18a5
Skenos Peniel <skenos3s@gmail.com>
2020-11-11 21:22:51 +0530

    added bootstrap styling

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,79 +1,96 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import logo from "../icon.png";
 
-import Navbar from './navbar.component'
-import Content from './content.component'
+import Navbar from "./navbar.component";
+import Content from "./content.component";
 
-import { getFromStorage } from '../utils/storage'; //implement log out
+import { getFromStorage } from "../utils/storage"; //implement log out
 
 export default class Home extends Component {
-    constructor() {
-        super()
-        this.handleNavigation=this.handleNavigation.bind(this)
-        this.state = {
-            signedin:false,
-            loading: false,
-            data: {},
-            token:0,
-            nav:'accidentsubmission'
-        }
-    }
+  constructor() {
+    super();
+    this.handleNavigation = this.handleNavigation.bind(this);
+    this.state = {
+      signedin: false,
+      loading: false,
+      data: {},
+      token: 0,
+      nav: "accidentsubmission",
+    };
+  }
 
-    componentDidMount() {
-        this.setState({loading: true})
-        const token = getFromStorage('road_accident_prevention_system_webtoken');
-        console.log("token:"+token);
-        axios.get("http://localhost:5000/police/verifysession/",{params:{token:token}})
-            .then(response => response.data)
-            .then(data => {
-                console.log(data);
-                this.setState({
-                    signedin:true,
-                    loading: false,
-                    data: data,
-                    token:token
-                })
-            })
-    }
+  componentDidMount() {
+    this.setState({ loading: true });
+    const token = getFromStorage("road_accident_prevention_system_webtoken");
+    console.log("token:" + token);
+    axios
+      .get("http://localhost:5000/police/verifysession/", {
+        params: { token: token },
+      })
+      .then((response) => response.data)
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          signedin: true,
+          loading: false,
+          data: data,
+          token: token,
+        });
+      });
+  }
 
-    handleNavigation(str){
-      this.setState({nav:str})
-    }
+  handleNavigation(str) {
+    this.setState({ nav: str });
+  }
 
-    render() {
-        if(this.state.loading){
-          return(
-            <div>
-              loading...
-            </div>)
-        }else{
-          //not logged in
-          if(!this.state.data.username){
-            return(
-              <div className="content">
-              <h1>Road Accident Prediction System</h1>
-              Sign in to continue<br/>
-              <Link to="/signin">Sign in</Link>
+  render() {
+    if (this.state.loading) {
+      return <div>loading...</div>;
+    } else {
+      //not logged in
+      if (!this.state.data.username) {
+        return (
+          <div className="content">
+            <nav className="navbar navbar-dark bg-dark">
+              <span className="navbar-brand mb-0 h1">
+                <img
+                  src={logo}
+                  width="30"
+                  height="30"
+                  className="d-inline-block align-top"
+                />{" "}
+                Road Accident Prediction System
+              </span>
+            </nav>
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Sign in to continue</h5>
+
+                <br />
+                <button className="btn btn-warning">
+                  <Link to="/signin">Sign in</Link>
+                </button>
               </div>
-            )
-          }else{
-          return (
-              <div>
-              <Navbar
-                adminRights={this.state.data.adminRights}
-                token={this.state.token}
-                navState={this.state.nav}
-                handleNavigation={this.handleNavigation}
-              />
-              <div className="content">
-                  <Content nav={this.state.nav} token={this.state.token}/>
-              </div>
-              </div>
-          )
-        }
-        }
-
-
+            </div>
+          </div>
+        );
+      } else {
+        return (
+          <div>
+            <Navbar
+              adminRights={this.state.data.adminRights}
+              token={this.state.token}
+              navState={this.state.nav}
+              handleNavigation={this.handleNavigation}
+            />
+            <div className="content">
+              <Content nav={this.state.nav} token={this.state.token} />
+            </div>
+          </div>
+        );
+      }
     }
+  }
 }
