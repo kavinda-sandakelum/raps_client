c9d04d04bf8ba1b087c1c87a9e77ed89de5709d7
Kavinda <kavinda.sandakelum20@gmail.com>
2021-03-24 22:24:12 +0530

    home.component UI

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import logo from "../icons/icon.png";
+import "./css/home.component.css";
+import { Container, Col, Row } from "react-bootstrap";
+import SVGsign1 from "../Assets/signin1.svg";
+import { motion } from "framer-motion";
 
 import Navbar from "./navbar.component";
 import Content from "./content.component";
@@ -50,7 +54,14 @@ export default class Home extends Component {
       return (
         <div className="loading">
           <svg>
-            <circle r="40" cx="150" cy="75" stroke="#999" stroke-width="10px" fill="none"/>
+            <circle
+              r="40"
+              cx="150"
+              cy="75"
+              stroke="#999"
+              stroke-width="10px"
+              fill="none"
+            />
           </svg>
         </div>
       );
@@ -59,30 +70,46 @@ export default class Home extends Component {
       if (!this.state.data.username) {
         return (
           <div className="content">
-            <nav className="navbar navbar-dark bg-dark">
-              <span className="navbar-brand mb-0 h1">
-                <img
-                  src={logo}
-                  width="30"
-                  height="30"
-                  className="d-inline-block align-top"
-                  alt="RAPS logo"
-                />&nbsp;
-                Road Accident Prediction System
-              </span>
-            </nav>
-
-
-            <div className="jumbotron jumbotron-fluid">
-              <div className="container">
-                <h1 className="display-4">Sign in to continue</h1>
-                <p class="lead">
-                  <button class="btn btn-warning btn-lg">
-                    <Link to="/signin">Sign in</Link>
-                  </button>
-                </p>
-
-              </div>
+            <div className="background1">
+              <motion.div
+                className="box-one"
+                initial={{ scale: 0.5, opacity: 0 }}
+                animate={{
+                  scale: 1,
+                  opacity: 1,
+                }}
+                transition={{ duration: 0.5 }}
+              >
+                <div className="container">
+                  <Row>
+                    <Col className="welcome1">
+                      <h2>Welcome to</h2>
+                      <h3>Road Accident Prevention System!</h3>
+                      <p className="desc">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud anim
+                        id est laborum.
+                      </p>
+                      <p class="lead">
+                        <h6>Let's get started</h6>
+                        <motion.button
+                          whileHover={{ scale: 1.05 }}
+                          class="btn btn-warning btn-lg"
+                        >
+                          <Link className="link" to="/signin">
+                            Sign in
+                          </Link>
+                        </motion.button>
+                      </p>
+                    </Col>
+                    <Col>
+                      <img className="signin-img-1" src={SVGsign1} />
+                    </Col>
+                    <Col></Col>
+                  </Row>
+                </div>
+              </motion.div>
             </div>
           </div>
         );
